20757eb2f6c335a8ec614795374bd80071489f3c
ksashik01 <kscomplainsheet@gmail.com>
2023-02-26 01:45:41 +0600

    Home-File

--- Moduale-34-API-Example/countries.js
+++ Moduale-34-API-Example/countries.js
@@ -7,20 +7,13 @@ fetch ('https://restcountries.com/v3.1/all')
 }
 
 
-
-
-
-
 const displayCountries = countries => {
 
     const contriesContainer= document.getElementById ('all-countries')
 
-// console.log (countries);
-// for (const country of countries)
-// console.log (country)
 
 countries.forEach(country => {
-    console.log (country.name.common)
+    console.log (country.cca2)
 
     const countryDiv = document.createElement ('div');
     countryDiv.classList.add ('country');
@@ -28,18 +21,38 @@ countries.forEach(country => {
    countryDiv.innerHTML =`
    
   <h3> Name:${country.name.common} </h3>
-  <p>Capital:${country.capital?country.capital [0]: 'no capital' } </p>
+  <p>Capital:${country.capital ? country.capital[0] : 'no capital' } </p>
+  <button onclick="displayCountryDetails('${country.cca2}')">Details</button>
    
    `
    contriesContainer.appendChild(countryDiv);
     
 });
-
+    
 }
 
-
-
-
-
+ const displayCountryDetails = code => {
+      
+    //    https://restcountries.com/v3.1/alpha/{code}
+     const url =`https://restcountries.com/v3.1/alpha/${code}`
+    //  console.log(url)
+     
+     fetch(url)
+         .then(res => res.json())
+        .then (data => showdCountryDetails(data[0]))
+      
+}  
+const showdCountryDetails = country => {
+    console.log (country)
+      
+    const detailContainer = document.getElementById('county-details')
+    detailContainer.innerHTML = `
+    
+<h3>Name:${country.name.common}</h3>
+<img src ="${country.flags.png}">
+    
+    `
+  }
+    
 
 loadCountries()
