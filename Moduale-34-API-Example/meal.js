da08328aacb6ad1e96ab7729dacd0c6697e087bc
ksashik01 <complainsheet@gmail.com>
2023-02-26 16:40:48 +0600

    office-file

---
+++ Moduale-34-API-Example/meal.js
@@ -0,0 +1,127 @@
+// https://themealdb.com/api.php
+
+const loadMeals = (searchtext)=> {
+const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchtext}`
+console.log (url)
+
+
+fetch (url)
+.then (res => res.json ())
+.then (data =>  displayMeals(data.meals))
+
+}
+
+const displayMeals = meals => {
+    const mealsContainer = document.getElementById ('meals-container')
+    mealsContainer.innerText='';
+
+meals.forEach(meal =>{
+
+    console.log (meal)
+    // step 2: creat child for each element--------
+    const mealDiv= document.createElement('div');
+    mealDiv.classList.add('col')
+
+    // step-3 set content of the child
+    mealDiv.innerHTML =`
+    
+<div class="card">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+
+        <button onclick = "loadMealDetails2(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+        Launch demo modal
+      </button>
+
+
+    </div>
+    </div>
+    `
+
+// step-4:appendChild
+
+mealsContainer.appendChild(mealDiv)
+
+})
+
+
+
+
+}
+
+
+
+
+
+
+const serachMeals = () => {
+
+    const searchText =document.getElementById 
+    ('search-field').value ;
+// search meals-------
+
+    console.log (searchText)
+    loadMeals (searchText)
+}
+
+
+const loadMealDetails = idMeal =>{
+
+    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}
+    `
+    fetch (url)
+    .then (res => res.json())
+    .then (data => displayMealsDetails(data.meals[0]))
+    .catch (error => {
+        // TypeError: Failed to fetch
+        console.log(error);
+      })
+   
+
+
+}
+
+
+const loadMealDetails2 = async(idMeal) =>{
+    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+   
+try{
+
+    const res = await fetch (url);
+    const data = await res.json ();
+    displayMealsDetails (data.meals[0]);
+
+}
+catch (error){
+    console.log (error)
+}
+
+
+}
+
+
+
+
+
+
+const displayMealsDetails = meal => {
+
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+
+    const mealsDetailst = document.getElementById ("mealDetailBody");
+
+    mealsDetailst.innerHTML =`
+    
+    <img class="w-50" src ="${meal.strMealThumb}">
+    
+    `
+
+
+}
+
+
+
+loadMeals('fish')
