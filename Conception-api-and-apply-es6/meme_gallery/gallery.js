82085e79157c46be8972d812bf08e338f9b44eae
ksashik01 <complainsheet@gmail.com>
2023-02-27 19:44:29 +0600

    new-

--- Conception-api-and-apply-es6/meme_gallery/gallery.js
+++ Conception-api-and-apply-es6/meme_gallery/gallery.js
@@ -25,22 +25,39 @@
 
 // loadMeme();
 
-
-
-
 const loadMeme = () => {
   const url = "https://meme-api.com/gimme/50";
   fetch(url)
     .then((res) => res.json())
     .then ((data) => showMeme(data.memes))
 
+
 }
-const showMeme = (hello) => {
+
+const showMeme = (memes) => {
+memes.slice (0, 10) .forEach((meme) => {
   
-console.log (hello)
+;
+  console.log (meme)
+  const memeContainer = document.getElementById("section")
+const div =document.createElement("div");
+div.innerHTML=`
+<div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img class="w-full h-60" src="${meme.url}" alt="Shoes" /></figure>
+  <div class="card-body">
+    
+   
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>`
 
-}
+memeContainer.appendChild(div)
+  
+;
+}) 
 
-
+};
 
 loadMeme();
--- Conception-api-and-apply-es6/world_tour/worldtour.js
+++ Conception-api-and-apply-es6/world_tour/worldtour.js
@@ -1,94 +1,170 @@
-// console.log("touring");
+// // console.log("touring");
 
-// get all data and pass only some data using slice to show in the UI
-// const loadAllData = () => {
+// // get all data and pass only some data using slice to show in the UI
+// // const loadAllData = () => {
+// //   fetch("https://restcountries.com/v3.1/all")
+// //     .then((res) => res.json())
+// //     .then((data) => {
+// //       //   console.log(data);
+// //       allData = data;
+// //       showAllData(data.slice(0, 5));
+// //     });
+// // };
+
+// // loadAllData()
+
+// // get all data and pass only some data using slice to show in the UI
+// const loadMyData = async () => {
+//   const res = await fetch("https://restcountries.com/v3.1/all");
+//   const data = await res.json();
+//   console.log(data);
+//   showAllData(data.slice(0, 5));
+// };
+// loadMyData();
+
+// // show data to UI
+// const showAllData = (countries) => {
+//   const countryContainer = document.getElementById("countries-info");
+//   countryContainer.innerHTML = "";
+//   countries.forEach((country) => {
+//     // console.log(country);
+//     const div = document.createElement("div");
+//     div.innerHTML = `
+//     <div class="card w-full h-96 bg-base-100 shadow-2xl">
+//     <figure class="px-10 pt-10">
+//       <img src="${country.flags.png}" alt="Shoes" class="rounded-xl" />
+//     </figure>
+//     <div class="card-body items-center text-center">
+//       <h2 class="card-title">${country.name.common}</h2>
+//       <p>Population: ${country.population}</p>
+//       <p>Currency: ${Object.keys(country.currencies)[0]}</p>
+//       <div class="card-actions">
+//         <label onclick="showSingleCountry('${
+//           country.cca2
+//         }')" class="btn btn-primary" for="my-modal-3" class="btn">Show Details</label>
+//       </div>
+//     </div>
+//   </div>
+//     `;
+//     countryContainer.appendChild(div);
+//   });
+// };
+
+// // get all data and pass all data using slice to show in the UI when click See All button
+// const showAllDataTogether = () => {
 //   fetch("https://restcountries.com/v3.1/all")
 //     .then((res) => res.json())
 //     .then((data) => {
-//       //   console.log(data);
-//       allData = data;
-//       showAllData(data.slice(0, 5));
+//       showAllData(data);
 //     });
 // };
 
-// loadAllData()
+// // get single country info using dynamic URL and unique id
+// const showSingleCountry = (id) => {
+//   const URL = `
+//   https://restcountries.com/v3.1/alpha/${id}`;
+//   fetch(URL)
+//     .then((res) => res.json())
+//     .then((data) => showSingleCountryDataModal(data[0]));
+// };
 
-// get all data and pass only some data using slice to show in the UI
-const loadMyData = async () => {
-  const res = await fetch("https://restcountries.com/v3.1/all");
-  const data = await res.json();
-  console.log(data);
-  showAllData(data.slice(0, 5));
-};
-loadMyData();
-
-// show data to UI
-const showAllData = (countries) => {
-  const countryContainer = document.getElementById("countries-info");
-  countryContainer.innerHTML = "";
-  countries.forEach((country) => {
-    // console.log(country);
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <div class="card w-full h-96 bg-base-100 shadow-2xl">
-    <figure class="px-10 pt-10">
-      <img src="${country.flags.png}" alt="Shoes" class="rounded-xl" />
-    </figure>
-    <div class="card-body items-center text-center">
-      <h2 class="card-title">${country.name.common}</h2>
-      <p>Population: ${country.population}</p>
-      <p>Currency: ${Object.keys(country.currencies)[0]}</p>
-      <div class="card-actions">
-        <label onclick="showSingleCountry('${
-          country.cca2
-        }')" class="btn btn-primary" for="my-modal-3" class="btn">Show Details</label>
-      </div>
-    </div>
-  </div>
-    `;
-    countryContainer.appendChild(div);
-  });
-};
-
-// get all data and pass all data using slice to show in the UI when click See All button
-const showAllDataTogether = () => {
-  fetch("https://restcountries.com/v3.1/all")
-    .then((res) => res.json())
-    .then((data) => {
-      showAllData(data);
-    });
-};
-
-// get single country info using dynamic URL and unique id
-const showSingleCountry = (id) => {
-  const URL = `
-  https://restcountries.com/v3.1/alpha/${id}`;
-  fetch(URL)
-    .then((res) => res.json())
-    .then((data) => showSingleCountryDataModal(data[0]));
-};
-
-// show single country info in modal using dynamic URL and unique id
-const showSingleCountryDataModal = (value) => {
-  const container = document.getElementById("modal-info");
-  const div = document.createElement("div");
-  div.classList.add("modal");
-  div.innerHTML = `
+// // show single country info in modal using dynamic URL and unique id
+// const showSingleCountryDataModal = (value) => {
+//   const container = document.getElementById("modal-info");
+//   const div = document.createElement("div");
+//   div.classList.add("modal");
+//   div.innerHTML = `
   
-  <div class="modal-box relative">
-  <label
-    for="my-modal-3"
-    class="btn btn-sm btn-circle absolute right-2 top-2"
-    >✕</label
-  >
-  <img src="${value.flags.png}" alt="">
-  <h3 class="text-lg font-bold">
-    ${value.name.common}
-  </h3>
-  <p class="py-4">
-    Population : ${value.population}
-  </p>
-</div>
-  `;
-  container.appendChild(div);
-};
+//   <div class="modal-box relative">
+//   <label
+//     for="my-modal-3"
+//     class="btn btn-sm btn-circle absolute right-2 top-2"
+//     >✕</label
+//   >
+//   <img src="${value.flags.png}" alt="">
+//   <h3 class="text-lg font-bold">
+//     ${value.name.common}
+//   </h3>
+//   <p class="py-4">
+//     Population : ${value.population}
+//   </p>
+// </div>
+//   `;
+//   container.appendChild(div);
+// };
+
+// const loadAllData =()=>{
+// fetch ("https://restcountries.com/v3.1/all")
+// .then ((res) => res.json())
+// .then ((data) =>{
+// console.log (data)
+// showAlldata (data.slice (0,3))
+// })
+ 
+// }
+
+// const showAlldata = (countries) => {
+
+
+//   const container= document.getElementById ("countries-info");
+//   container.innerHTML="";
+ 
+//   countries.forEach((country) => {
+//     // console.log (country.cca2);
+//   const div = document.createElement ("div");
+//   div.innerHTML=`
+  
+//   <div class="card w-96 bg-base-100 shadow-xl">
+//   <figure><img class="w-full h-60"  src="${country.flags.png}" alt="Shoes" /></figure>
+//   <div class="card-body">
+//     <h2 class="card-title">${country.name.common}</h2>
+//     <p>If a dog chews shoes whose shoes does he choose?</p>
+//     <div class="card-actions justify-end">
+//       <button onClick="showDetails('${country.cca2}')" class="btn btn-primary">Buy Now</button>
+//     </div>
+//   </div>
+// </div>
+  
+//   `
+//   container.appendChild (div)
+
+// });
+
+// }
+
+// const showDetails = (id) => {
+
+// const url =`https://restcountries.com/v3.1/alpha/${id}`;
+//  fetch (url)
+// .then ((res) =>res.json())
+// .then ((data) => console.log(data))
+
+// }
+
+// const showAllDataTogether = () => {
+//   fetch ("https://restcountries.com/v3.1/all")
+// .then ((res) => res.json())
+// .then ((data) =>{
+
+// showAlldata (data)
+// })
+
+// }
+
+
+// loadAllData ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
